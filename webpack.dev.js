466ace42a74ba4e4392a4d53457b02cf73f32014
Neven Đuranec <neven.duranec@gmail.com>
2021-10-19 14:03:23 +0200

    Fix dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -45,20 +45,6 @@ module.exports = {
                 ],
             },
             {
-                // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            // On development we want to see where the file is coming from, hence we preserve the [path]
-                            name: '[path][name].[ext]?hash=[hash:20]',
-                            limit: 8192,
-                        },
-                    },
-                ],
-            },
-            {
                 // Load all icons
                 test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
                 use: [
